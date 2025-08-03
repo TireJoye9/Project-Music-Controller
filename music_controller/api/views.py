f5d3bd369e72420f7c08c488af317d292b10d656
TireJoye <tirenioluwa.aruwajoye@ucdconnect.ie>
2025-08-03 18:58:30 +0100

    Set up serialisers, views and models.

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -6,6 +6,6 @@ from .models import Room
 # Create your views here.
 #Api view to let us view list of all the different rooms
 
-class RoomView(generics.CreateAPIView): #allows us to view all different rooms and create the rooms too
+class RoomView(generics.ListAPIView): #allows us to view all different rooms and create the rooms too
     queryset = Room.objects.all()
     serializer_class = RoomSerializers
