8ad1f7b5cd4c6efcff5403719a2847e26e2ee2d8
TireJoye <tirenioluwa.aruwajoye@ucdconnect.ie>
2025-08-31 19:12:02 +0100

    Created join room page, connected it to backend developed post request etc, Gonna have to change to functions and hooks, as Im having to rewrite the navigate wrapper for each page its looking like.

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import RoomView
 from .views import CreateRoomView
-from.views import GetRoom
-
+from .views import GetRoom
+from .views import JoinRoom
 urlpatterns = [
     path('room', RoomView.as_view()),
     path('create', CreateRoomView.as_view()),
-    path('get-room', GetRoom.as_view())
+    path('get-room', GetRoom.as_view()),
+    path('join', JoinRoom.as_view()),
 ]
--- music_controller/frontend/src/components/RoomJoinPage.js
+++ music_controller/frontend/src/components/RoomJoinPage.js
@@ -1,15 +1,89 @@
 import React, { Component } from 'react';
+import { TextField, Button, Grid, Typography } from '@material-ui/core';
+import {Link} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-export default class RoomJoinPage extends Component {
+
+
+
+class RoomJoinPage extends Component {
     constructor(props) {
         super(props);
+        this.state= {
+            roomCode: "",
+            error: "",
+        }
+        this.handleTextFieldChange = this.handleTextFieldChange.bind(this);
+        this.roomButtonPressed = this.roomButtonPressed.bind(this);
     }
+    
 
      render() {
     return (
-        <div className='center'>
-            <p>This is the Join Room Page</p>
-        </div>
+        <Grid container spacing={1}>
+            <Grid item xs={12} align="center">
+                <Typography variant="h4" component="h4">
+                    Join a Room
+                </Typography>
+            </Grid>
+            <Grid item xs={12} align="center">
+                <TextField
+                error={this.state.error}
+                label="Code"
+                placeholder="Enter a Room Code"
+                value={this.state.roomCode}
+                helperText={this.state.error}
+                variant="outlined"
+                onChange={this.handleTextFieldChange}
+                />
+            </Grid>
+             <Grid item xs={12} align="center">
+                  <Button variant='contained' color="primary" onClick={this.roomButtonPressed}>
+                    Enter Room
+                  </Button>
+             </Grid>
+              <Grid item xs={12} align="center">
+                <Button variant='contained' color="secondary" to="/" component={Link}>Back
+                </Button>
+              </Grid>
+        </Grid>
+        
     );
   }
+
+  handleTextFieldChange(e) {
+    this.setState({
+        roomCode: e.target.value,
+    })
+  }
+  //post request to backend to check if room exists
+  roomButtonPressed() {
+    const requestOptions = {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            code: this.state.roomCode
+        })
+    };
+    fetch('/api/join', requestOptions).then((response) => {
+        if (response.ok){
+            this.props.navigate(`/room/${this.state.roomCode}`)
+        } else {
+            this.setState({error: "Room not found."})
+        }
+    }).catch((error) => {
+        console.log(error);
+    });
+  }
 }
+
+//copied straight from my CreateRoomPage
+// Wrapper to inject navigate
+//  might change to function as inject wrappers for my classes is a bit redundant
+function WithNavigate(props) {
+  let navigate = useNavigate();
+  return <RoomJoinPage {...props} navigate={navigate} />;
+}
+
+export default WithNavigate;
+
